644b5f373ab2dd07469178701e6f7f6652e0207a
DevFlock <37045958+DevFlock@users.noreply.github.com>
2022-07-24 23:16:20 +0100

    Started making server, made Client class.
    The Client class connects the client to the server,
    then get's their name from the query params and if it cannot be found,
    then the client will be given the name "Anonymous"

---
+++ server/main.py
@@ -0,0 +1,14 @@
+from classes import ConnectionManager
+from fastapi import FastAPI, WebSocket
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+conn_manager = ConnectionManager()
+
+app.mount("/", StaticFiles(directory="../client", html=True), name="root")
+
+
+@app.route("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    """The main endpoint for the websocket."""
+    conn_manager.add_connection(websocket)
