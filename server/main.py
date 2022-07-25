ab23b2d357368c272fe5dc6e37a4c338089c6bbf
Maxim Ivanov (github username: maximivanov1711) <maxim.ivanov1711@gmail.com>
2022-07-25 16:21:27 +0300

    Returned to ws

--- server/main.py
+++ server/main.py
@@ -194,7 +194,7 @@ manager = ConnectionManager()
 game = GameManager()
 
 
-@app.websocket("/wss")
+@app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     is_player_connected = await manager.connect(websocket)
 
