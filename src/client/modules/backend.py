cae59d1b82bf4a9e11e22b97c2b92a3f06098ed4
Konrad (PC) <konrad@guzek.uk>
2022-07-27 01:08:22 +0200

    add http scheme to request if not present

--- src/client/modules/backend.py
+++ src/client/modules/backend.py
@@ -8,8 +8,14 @@ session = aiohttp.ClientSession()
 
 async def test_connection(url: str):
     """Tests if the request returns an OK response."""
+    url = url.rstrip("/") + "/test-connection"
+    # Add HTTP scheme if not present
+    if "://" not in url:
+        url = "http://" + url.lstrip(":/")
+    print("GET", url)
     try:
         async with session.get(url) as resp:
+            print(resp)
             return resp.ok
     except aiohttp.ClientConnectionError:
         return False
