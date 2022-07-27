17bfda983266a8b55986cae40f436d21867cd858
Konrad (PC) <konrad@guzek.uk>
2022-07-27 02:35:24 +0200

    merge branch `basic-server`

--- src/server/main.py
+++ src/server/main.py
@@ -1,208 +1,59 @@
-"""Entry point for the application websocket server."""
-
-from asyncio import gather
-from typing import List
+"""Websocket definitions."""
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse, JSONResponse
 
-# from fastapi.staticfiles import StaticFiles
-
-
-def debug(*args):
-    """Log the message if debug mode is enabled."""
-    if DEBUG:
-        print(*args)
-
-
-DEBUG = True
-
+from server.wsserver import ConnectionManager, Message, EventType
 
 app = FastAPI()
+conn_manager = ConnectionManager()
 
-# Adding access to client.html
-# app.mount("/", StaticFiles(directory="../client", html=True), name="client")
-
-
-class ConnectionManager:
-    """Code relating to connecting to the websocket server."""
-
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-        self.connected_player = None
-
-    async def connect(self, websocket: WebSocket):
-        """Creates a new connection between the server and the client."""
-        await websocket.accept()
-
-        if len(self.active_connections) == 2:
-            await websocket.close(4000)
-
-            return False
-
-        self.active_connections.append(websocket)
-
-        if len(self.active_connections) == 1:
-            await websocket.send_json({"type": "connection", "player": "x"})
-
-            self.connected_player = "x"
-        elif len(self.active_connections) == 2:
-            await websocket.send_json({"type": "connection", "player": "o"})
-
-            await self.active_connections[0].send_json({"type": "start"})
-
-            self.connected_player = "o"
-        return True
-
-    async def disconnect(self, websocket: WebSocket):
-        """Disposes the websocket connection."""
-        self.active_connections.remove(websocket)
-
-        if self.active_connections:
-            await self.active_connections[0].send_json({"type": "disconnect"})
-
-    async def broadcast(self, message: dict):
-        """Sends the message to all active connections."""
-        coroutine = (conn.send_json(message) for conn in self.active_connections)
-        # Execute all coroutines in parallel
-        gather(*coroutine)
+app.mount("/client", StaticFiles(directory="client", html=True), name="client")
 
 
-class GameManager:
-    """Code relating to the game logic."""
-
-    def __init__(self):
-        """Initialises all properties to None, then sets the starting values in `reset()`."""
-        self.board = None
-        self.players = None
-        self.is_started = None
-        self.current_player = None
-        self.reset()
-
-    def add_player(self, player: str, websocket: WebSocket):
-        """Adds the websocket to the map of connected players."""
-        self.players[player] = websocket
-
-    def start(self):
-        """Starts the game, with player 'x' having the first move."""
-        self.is_started = True
-        self.current_player = "x"
-
-    async def process_turn(self, turn: dict):
-        """Handles the incoming turns and sends the corresponding messages to players."""
-
-        debug(turn)
-
-        player = turn["player"]
-        cell = turn["cell"]
-
-        if self.current_player != player:
-            await manager.broadcast(
-                {"type": "denied", "player": player, "message": "Wait for your turn"}
-            )
-            return
-
-        cell_row, cell_col = divmod(cell, 3)
-
-        if self.board[cell_row][cell_col] != "*":
-            await manager.broadcast(
-                {
-                    "type": "denied",
-                    "player": player,
-                    "message": "You can't move in this cell",
-                }
-            )
-            return
-
-        await manager.broadcast({"type": "turn", "player": player, "cell": cell})
-
-        self.board[cell_row][cell_col] = player
-
-        # Check if there is a player who won.
-        win = self.check_win()
-        if win:
-            await manager.broadcast(
-                {"type": "win", "winner": self.current_player, "cells": win}
-            )
-
-            self.reset()
-
-            return
-
-        self.toggle_current_player()
-
-    def toggle_current_player(self):
-        """Toggles the current player, which is asked to make a move."""
-
-        if self.current_player == "x":
-            self.current_player = "o"
-
-        elif self.current_player == "o":
-            self.current_player = "x"
-
-    @property
-    def is_ready(self):
-        """Returns a boolean indicating if the number of players connected is 2."""
-        return len(self.players) == 2
-
-    def check_win(self):
-        """Checks if the current player is won and, if so,
-        returns cells that led to the win. Otherwise returns False."""
-
-        # Check if current player won by filling one of the rows
-        for row_i, row in enumerate(self.board):
-            if row.count(self.current_player) == 3:
-                return (row_i * 3, row_i * 3 + 1, row_i * 3 + 2)
-
-        # Check if current player won by filling one of the columns
-        columns = [[self.board[i][j] for i in range(3)] for j in range(3)]
-        for col_i, col in enumerate(columns):
-            if col.count(self.current_player) == 3:
-                return (col_i, col_i + 3, col_i + 6)
-
-        # Check if current player won by filling first diagonal
-        if all(self.board[i][i] == self.current_player for i in range(3)):
-            return (0, 4, 8)
-
-        # Check if current player won by filling second diagonal
-        if all(self.board[i][2 - i] == self.current_player for i in range(3)):
-            return (0, 4, 6)
-
-        # Otherwise, current player didn`t won, so return False
-        return False
-
-    def reset(self):
-        """Resets the game to the defaults."""
-        self.board = [["*"] * 3] * 3
-        self.players = {}
-        self.is_started = False
-        self.current_player = ""
+@app.get("/")
+async def root():
+    """Redirects to the `/client` subpath containing the client static page content."""
+    return RedirectResponse(url="/client")
 
 
-manager = ConnectionManager()
-game = GameManager()
+@app.get("/test-connection")
+async def test():
+    """Endpoint to validate if the server is running."""
+    return JSONResponse({"server-running": True})
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    """Main websocket endpoint for the client connection to the server."""
-    is_player_connected = await manager.connect(websocket)
+    """The main endpoint for the websocket."""
+    client = await conn_manager.connect(websocket)
 
-    # If the game is already started, then exit
-    if not is_player_connected:
-        return
+    async def transfer_client(data):
+        try:
+            room_id = int(data.get("roomid"))
+        except (TypeError, ValueError):
+            # TypeError if data.get() evaluates to None; ValueError if it cannot be cast to int
+            return
 
-    game.add_player(manager.connected_player, websocket)
+        room = conn_manager.get_room(room_id)
 
-    if game.is_ready:
-        game.start()
+        await room.broadcast(Message(EventType.CONNECT, client=client.serialised))
+        await conn_manager.transfer_client(client, room)
 
     try:
         while True:
-            data = await websocket.receive_json()
+            data = await client.recieve_data()
+            print(data)
 
-            if game.is_started:
-                await game.process_turn(data)
+            match data["type"]:
+                case "transfer":
+                    await transfer_client(data)
+
+                case _:
+                    pass
+
+            print(conn_manager)
 
     except WebSocketDisconnect:
-        await manager.disconnect(websocket)
-        game.reset()
+        await conn_manager.disconnect(client)
---
+++ src/server/wsserver.py
@@ -0,0 +1,178 @@
+"""Entry point for the server application."""
+
+from asyncio import gather
+import re
+from enum import Enum
+from uuid import uuid4
+
+from fastapi import WebSocket
+
+
+class EventType(Enum):
+    """Enum containing all message event types."""
+
+    CONNECT = "new_connection"
+    TRANSFER = "transferred"
+    DISCONNECT = "client_disconnected"
+
+
+class Message:
+    """Message class to be used for server-client communication."""
+
+    def __init__(self, msg_type: EventType, **kwargs):
+        self.type = msg_type.value
+        self.options = kwargs
+
+    @property
+    def serialised(self) -> dict:
+        """Gets the message instance serialised as a dictionary."""
+        return {"type": self.type, **self.options}
+
+
+class Client:
+    """The main class for the client.
+
+    Gives the client a name and handles sending of data.
+    Name defaults to "Anonymous" if not provided.
+
+    Args:
+        WebSocket: The WebSocket object for the client.
+        name: The name of the client. Alphanumeric 3-20 characters. Default: Anonymous
+        room_id: ID of the client
+    """
+
+    def __init__(self, websocket: WebSocket, room_id: int = None):
+        self.name = websocket.query_params.get("name")
+        self.room_id = room_id
+        self.socket = websocket
+        self.uuid = str(uuid4())
+
+        if self.name:
+            # Checks to make sure the name is only using
+            # alphanumeric characters and is 3 to 20 characters long
+            if re.match(r"^[a-zA-Z0-9]{3,20}$", self.name):
+                self.name = self.name
+            else:
+                self.name = "Anonymous"
+        else:
+            self.name = "Anonymous"
+
+    async def send_message(self, message: Message):
+        """
+        Sends a message to the client.
+
+        Args:
+            message: The Message object to send.
+        """
+        await self.socket.send_json(message.serialised)
+
+    async def accept(self) -> None:
+        """Accepts the socket connection"""
+        await self.socket.accept()
+
+    async def recieve_data(self) -> dict:
+        """Recieves data send by the client"""
+        return await self.socket.receive_json()
+
+    async def close(self) -> None:
+        """Closes the conection to the server."""
+        print("Closed client", self.serialised)
+
+    @property
+    def serialised(self) -> dict:
+        """Gets a serialised version of the client instance."""
+        return {"uuid": self.uuid, "name": self.name}
+
+
+class Room:
+    """
+    Main class for a game room.
+
+    Each room has a list of clients (up to 2 unless we do spectators).
+    Also has an id which will probably be a uuid.
+    """
+
+    def __init__(self, room_id: int) -> None:
+        self.room_id = room_id
+        self.clients: list[Client] = []
+
+    def add_client(self, client: Client):
+        """Adds the client to the room."""
+        self.clients.append(client)
+
+    async def remove_client(self, client: Client):
+        """Removes the client from the room."""
+        self.clients.remove(client)
+        await self.broadcast(Message(EventType.DISCONNECT, uuid=client.uuid))
+
+    async def broadcast(self, message: Message):
+        """Sends the message to all connected clients."""
+        # Send all messages concurrently
+        coroutines = (client.send_message(message) for client in self.clients)
+        await gather(*coroutines)
+
+
+class ConnectionManager:
+    """
+    The main connection manager for the server
+
+    Keeps track of connections in self.connections and rooms in self.rooms
+    """
+
+    def __init__(self) -> None:
+        self.connections: list[WebSocket] = []
+        self.rooms: list[Room] = []
+
+    async def connect(self, websocket: WebSocket) -> Client:
+        """Connects the websocket to the server."""
+        client = Client(websocket, None)
+        print(websocket.query_params)
+        await client.accept()
+        self.connections.append(client)
+        return client
+
+    async def remove_client_from_room(self, client) -> None:
+        """Removes the client from its current room."""
+        if not client.room_id:
+            return
+        room = self.get_room(client.room_id)
+        await room.remove_client(client)
+
+        if len(room.clients) == 0:
+            self.rooms.remove(room)
+
+    async def disconnect(self, client: Client):
+        """Disconnects the client from the server."""
+        await client.close()
+
+        self.connections.remove(client)
+        await self.remove_client_from_room(client)
+
+    async def transfer_client(self, client: Client, room: Room):
+        """Transfers the client to the room."""
+        await self.remove_client_from_room(client)
+
+        if room not in self.rooms:
+            self.rooms.append(room)
+
+        room.add_client(client)
+        client.room_id = room.room_id
+
+        clients = [client.serialised for client in room.clients]
+        await client.send_message(
+            Message(EventType.TRANSFER, roomid=room.room_id, clients=clients)
+        )
+
+    def get_room(self, room_id: int) -> Room:
+        """Gets the room with the id or creates one."""
+        for room in self.rooms:
+            if room.room_id == room_id:
+                return room
+        return Room(room_id)
+
+    def __str__(self) -> str:
+        output = "Rooms:\n"
+        for room in self.rooms:
+            output += f"{room.room_id}: {', '.join(i.name for i in room.clients)}\n"
+
+        return output
