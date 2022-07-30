ca108e6603be55db19d173d00f5359c0551faaef
Konrad (PC) <konrad@guzek.uk>
2022-07-30 21:21:37 +0200

    specify watch directory for uvicorn dev mode

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $1 == "--reload" ]]; then
   echo "Started server in development mode!"
-  uvicorn server.main:app --host localhost --port 8000 --reload
+  uvicorn server.main:app --host localhost --port 8000 --reload --reload-dir ./server
 else
   echo "Started server in production mode!"
   uvicorn server.main:app --host localhost --port 8000
