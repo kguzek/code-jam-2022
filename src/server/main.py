4d4b368e554e81bf760474f02b38ac1fd4080b7e
Konrad (PC) <konrad@guzek.uk>
2022-07-31 02:46:39 +0200

    implement `playercount` and `log` messages

--- src/server/main.py
+++ src/server/main.py
@@ -9,11 +9,12 @@ from fastapi.staticfiles import StaticFiles
 DEBUG = True
 
 
-def debug(message):
+async def debug(websocket: WebSocket, message):
     """This function prints the debug message if DEBUG is set to True."""
 
-    if DEBUG:
-        print(message)
+    if not DEBUG:
+        return
+    await websocket.send_json({"type": "log", "body": message})
 
 
 class ConnectionManager:
@@ -26,22 +27,34 @@ class ConnectionManager:
         self.clients = []
 
     async def add_client(self, client: WebSocket):
+        """Adds the client to the list of connected clients."""
         self.clients.append(client)
 
-    async def get_open_rooms(self):
-        open_rooms = [
-            room_id
-            for room_id, room in self.rooms.items()
-            if None in [room["x"], room["o"]]
-        ]
+    async def get_open_rooms(self, websocket: WebSocket):
+        """Gets the rooms that are not full."""
+        open_rooms = []
+        room_descriptions = ["Rooms:"]
+        for room_id, room in self.rooms.items():
+            x_vacant = room["x"] is None
+            o_vacant = room["o"] is None
+            if x_vacant or o_vacant:
+                if x_vacant and o_vacant:
+                    self.rooms.pop(room_id)
+                else:
+                    open_rooms.append(room_id)
+            room_descriptions.append(
+                f"  {room_id}: {sum(map(bool, room.values())) } players"
+            )
+
+        await debug(websocket, "\n".join(room_descriptions))
 
         return open_rooms
 
-    async def update_open_rooms(self):
+    async def update_open_rooms(self, websocket: WebSocket):
         """This function sends information about new rooms to clients
         that are not connected to room."""
 
-        open_rooms = await self.get_open_rooms()
+        open_rooms = await self.get_open_rooms(websocket)
 
         for client in self.clients:
             await client.send_json(
@@ -60,7 +73,7 @@ conn_manager = ConnectionManager()
 
 @app.get("/")
 async def root():
-    """This function redirects to the `/client` subpath containing the client static page content."""
+    """This function redirects to the `/client` subpath containing the client page content."""
     return RedirectResponse(url="/client")
 
 
@@ -77,16 +90,14 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             message = await websocket.receive_json()
-            debug(f"Received message: {message}")
+            await debug(websocket, f"Received message '{message}'")
 
             match message["type"]:
                 # If client wants to connect to the room:
                 case "connect":
                     # Add client to the room.
                     room_id = message["room_id"]
-
                     room = conn_manager.rooms[room_id]
-
                     room["o"] = websocket
 
                     # Send message with type "connected" to the client.
@@ -109,11 +120,13 @@ async def websocket_endpoint(websocket: WebSocket):
 
                     await conn_manager.add_client(websocket)
 
-                    await conn_manager.update_open_rooms()
+                    await conn_manager.update_open_rooms(websocket)
+
+                    conn_manager.clients.remove(websocket)
 
                 # If message type is "get_open_rooms":
                 case "get_open_rooms":
-                    open_rooms = await conn_manager.get_open_rooms()
+                    open_rooms = await conn_manager.get_open_rooms(websocket)
 
                     await websocket.send_json(
                         {
@@ -140,12 +153,17 @@ async def websocket_endpoint(websocket: WebSocket):
                         }
                     )
 
-                    conn_manager.clients.remove(websocket)
+                    await conn_manager.update_open_rooms(websocket)
 
-                    await conn_manager.update_open_rooms()
+                case "get_playercount":
+                    # Send the number of connected players
+                    num_players = len(conn_manager.clients)
+                    await websocket.send_json(
+                        {"type": "playercount", "playercount": num_players}
+                    )
+                case _:
+                    await debug(websocket, "Invalid message type: " + message["type"])
 
     # If client has disconnected:
     except WebSocketDisconnect:
-        debug("Client disconnected")
-
         conn_manager.clients.remove(websocket)
